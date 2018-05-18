890b44814521fb8cfabefff5e8964ac57695d5a0
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-18 10:59:21 -0400

    finished school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+# class School
+#   attr_accessor :name :roster
+
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+
+#   def add_student(name, grade)
+
+#   end
+# end
+
+require 'pry'
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+    @roster[grade] << student_name
+    else
+    @roster[grade] = []
+    @roster[grade] << student_name
+    end
+  end
+
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+
+  def sort
+    @roster.each { |grade, names| names.sort! }
+  end
+end
+
